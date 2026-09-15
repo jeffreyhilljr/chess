9ad5974428a64c691455d4f7214fa59aa4f6c6b6
Jeffrey Hill <jeffreyhilljr@gmail.com>
2026-09-14 23:48:37 -0600

    Added Main in ChessPiece for testing

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -83,4 +83,8 @@ public class ChessPiece {
 
         return String.format("%c", c);
     }
+
+//    public static void main(String[] args) {
+//        ChessPiece whitePawn = new ChessPiece(...)
+//    }
 }
