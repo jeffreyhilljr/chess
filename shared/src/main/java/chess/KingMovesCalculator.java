1b3a1328cb0a9cd3229b1a465aa230012e0bdff6
Jeffrey Hill <jeffreyhilljr@gmail.com>
2026-09-15 23:12:06 -0600

    Added KingMovesCalculator

---
+++ shared/src/main/java/chess/KingMovesCalculator.java
@@ -0,0 +1,31 @@
+package chess;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KingMovesCalculator implements PieceMovesCalculator {
+
+    @Override
+    public List<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
+        List<ChessMove> moves = new ArrayList<>();
+
+        int row = position.getRow();
+        int col = position.getColumn();
+        ChessGame.TeamColor team = board.getPiece(position).getTeamColor();
+        ChessPiece.PieceType type = board.getPiece(position).getPieceType();
+
+        for (int r = row - 1; r <= row + 1; r++) {
+            for (int c = col - 1; c <= col + 1; c++) {
+                ChessPosition newPosition = new ChessPosition(r, c);
+                if (board.getPiece(newPosition) == null) {
+                    ChessMove newMove = new ChessMove(position, newPosition, null);
+                    moves.add(newMove);
+                } else if (board.getPiece(newPosition).getTeamColor() != team) {
+                    ChessMove newMove = new ChessMove(position, newPosition, null);
+                    moves.add(newMove);
+                }
+            }
+        }
+        return moves;
+    }
+}
