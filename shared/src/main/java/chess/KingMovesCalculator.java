7e43b0d1c74bf94df19ec148191927efadbd26d2
Jeffrey Hill <jeffreyhilljr@gmail.com>
2026-09-23 21:17:46 -0600

    Refactored KingMovesCalculator

--- shared/src/main/java/chess/KingMovesCalculator.java
+++ shared/src/main/java/chess/KingMovesCalculator.java
@@ -6,25 +6,18 @@ import java.util.List;
 public class KingMovesCalculator implements PieceMovesCalculator {
 
     @Override
-    public List<ChessMove> pieceMovesGetter(ChessBoard board, ChessPosition position) {
+    public List<ChessMove> pieceMovesGetter(ChessBoard board, ChessPosition myPosition) {
         List<ChessMove> moves = new ArrayList<>();
 
-        int row = position.getRow();
-        int col = position.getColumn();
-        ChessGame.TeamColor team = board.getPiece(position).getTeamColor();
-        ChessPiece.PieceType type = board.getPiece(position).getPieceType();
+        int row = myPosition.getRow();
+        int col = myPosition.getColumn();
+
 
         for (int r = row - 1; r <= row + 1; r++) {
             for (int c = col - 1; c <= col + 1; c++) {
-                if (r > 0 && r < 9 && c > 0 && c < 9) {
+                if (inBounds(r, c)) {
                     ChessPosition newPosition = new ChessPosition(r, c);
-                    if (board.getPiece(newPosition) == null) {
-                        ChessMove newMove = new ChessMove(position, newPosition, null);
-                        moves.add(newMove);
-                    } else if (board.getPiece(newPosition).getTeamColor() != team) {
-                        ChessMove newMove = new ChessMove(position, newPosition, null);
-                        moves.add(newMove);
-                    }
+                    addMoveIfValid(board, myPosition, newPosition, moves);
                 }
             }
         }
