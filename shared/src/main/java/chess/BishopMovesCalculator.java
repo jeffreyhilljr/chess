42f4b3c8e8c4d5b47d819cf5fb00b83f9152b9fb
Jeffrey Hill <jeffreyhilljr@gmail.com>
2026-09-23 21:42:36 -0600

    Refactored BishopMovesCalculator

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -6,101 +6,13 @@ import java.util.List;
 public class BishopMovesCalculator implements PieceMovesCalculator {
 
     @Override
-    public List<ChessMove> pieceMovesGetter(ChessBoard board, ChessPosition position) {
+    public List<ChessMove> pieceMovesGetter(ChessBoard board, ChessPosition myPosition) {
         List<ChessMove> moves = new ArrayList<>();
 
-        int row = position.getRow();
-        int col = position.getColumn();
-        ChessGame.TeamColor team = board.getPiece(position).getTeamColor();
-        ChessPiece.PieceType type = board.getPiece(position).getPieceType();
-
-        int r = row;
-        int c = col;
-        while(true) {
-            r++;
-            c++;
-            if (r > 0 && r < 9 && c > 0 && c < 9) {
-                ChessPosition newPosition = new ChessPosition(r, c);
-                if (board.getPiece(newPosition) == null) {
-                    ChessMove newMove = new ChessMove(position, newPosition, null);
-                    moves.add(newMove);
-                } else if (board.getPiece(newPosition).getTeamColor() != team) {
-                    ChessMove newMove = new ChessMove(position, newPosition, null);
-                    moves.add(newMove);
-                    break;
-                } else {
-                    break;
-                }
-            } else {
-                break;
-            }
-        }
-
-        r = row;
-        c = col;
-        while(true) {
-            r++;
-            c--;
-            if (r > 0 && r < 9 && c > 0 && c < 9) {
-                ChessPosition newPosition = new ChessPosition(r, c);
-                if (board.getPiece(newPosition) == null) {
-                    ChessMove newMove = new ChessMove(position, newPosition, null);
-                    moves.add(newMove);
-                } else if (board.getPiece(newPosition).getTeamColor() != team) {
-                    ChessMove newMove = new ChessMove(position, newPosition, null);
-                    moves.add(newMove);
-                    break;
-                } else {
-                    break;
-                }
-            } else {
-                break;
-            }
-        }
-
-        r = row;
-        c = col;
-        while(true) {
-            r--;
-            c++;
-            if (r > 0 && r < 9 && c > 0 && c < 9) {
-                ChessPosition newPosition = new ChessPosition(r, c);
-                if (board.getPiece(newPosition) == null) {
-                    ChessMove newMove = new ChessMove(position, newPosition, null);
-                    moves.add(newMove);
-                } else if (board.getPiece(newPosition).getTeamColor() != team) {
-                    ChessMove newMove = new ChessMove(position, newPosition, null);
-                    moves.add(newMove);
-                    break;
-                } else {
-                    break;
-                }
-            } else {
-                break;
-            }
-        }
-
-        r = row;
-        c = col;
-        while(true) {
-            r--;
-            c--;
-            if (r > 0 && r < 9 && c > 0 && c < 9) {
-                ChessPosition newPosition = new ChessPosition(r, c);
-                if (board.getPiece(newPosition) == null) {
-                    ChessMove newMove = new ChessMove(position, newPosition, null);
-                    moves.add(newMove);
-                } else if (board.getPiece(newPosition).getTeamColor() != team) {
-                    ChessMove newMove = new ChessMove(position, newPosition, null);
-                    moves.add(newMove);
-                    break;
-                } else {
-                    break;
-                }
-            } else {
-                break;
-            }
-        }
+        moveUntilStopped(board, myPosition, 1, 1, moves);
+        moveUntilStopped(board, myPosition, -1, 1, moves);
+        moveUntilStopped(board, myPosition, 1, -1, moves);
+        moveUntilStopped(board, myPosition, -1, -1, moves);
 
         return moves;
     }
