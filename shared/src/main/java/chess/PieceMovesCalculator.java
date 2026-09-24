f52c5443851ed21e3aa200a5225ecc2f8d6b46b8
Jeffrey Hill <jeffreyhilljr@gmail.com>
2026-09-23 21:41:13 -0600

    Added moveUntilStopped helper to PieceMovesCalculator

--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.List;
+import java.util.function.UnaryOperator;
 
 public interface PieceMovesCalculator {
     List<ChessMove> pieceMovesGetter(ChessBoard board, ChessPosition myPosition);
@@ -15,6 +16,22 @@ public interface PieceMovesCalculator {
                 board.getPiece(newPosition).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
             ChessMove newMove = new ChessMove(myPosition, newPosition, null);
             moves.add(newMove);
+        }
+    }
+
+    default void moveUntilStopped(ChessBoard board, ChessPosition myPosition,
+                                  int rowDirection, int colDirection, List<ChessMove> moves) {
+        int r = myPosition.getRow();
+        int c = myPosition.getColumn();
+
+        while (inBounds(r + rowDirection, c + colDirection)) {
+            r += rowDirection;
+            c += colDirection;
+            ChessPosition newPosition = new ChessPosition(r, c);
+            addMoveIfValid(board, myPosition, newPosition, moves);
+            if (board.getPiece(newPosition) != null) {
+                break;
+            }
         }
     }
 }
