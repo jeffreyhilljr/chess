64b3224f4e155daf8d1ccfa12c09ef4feeef09f4
Jeffrey Hill <jeffreyhilljr@gmail.com>
2026-09-23 21:15:15 -0600

    Added default helper methods to PieceMovesCalculator

--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -3,5 +3,18 @@ package chess;
 import java.util.List;
 
 public interface PieceMovesCalculator {
-    List<ChessMove> pieceMovesGetter(ChessBoard board, ChessPosition position);
+    List<ChessMove> pieceMovesGetter(ChessBoard board, ChessPosition myPosition);
+
+    default boolean inBounds(int row, int col) {
+        return row > 0 && row < 9 && col > 0 && col < 9;
+    }
+
+    default void addMoveIfValid(ChessBoard board, ChessPosition myPosition,
+                                ChessPosition newPosition, List<ChessMove> moves) {
+        if (board.getPiece(newPosition) == null ||
+                board.getPiece(newPosition).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
+            ChessMove newMove = new ChessMove(myPosition, newPosition, null);
+            moves.add(newMove);
+        }
+    }
 }
